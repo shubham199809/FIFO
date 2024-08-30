4d83f82f89e41381e3fd1ac58e47ab15801caa9d
shubham199809 <shubham.sml9@gmail.com>
2024-08-30 13:29:06 +0530

    Update Asyncronous_fifo.v

--- Asyncronous_fifo.v
+++ Asyncronous_fifo.v
@@ -1,106 +1,79 @@
+module asynchronous_fifo(trans_clk,trans_rst,write_enable,recv_clk,recv_rst,read_enable,fifo_full,fifo_empty,trans_data,recv_data);
 
-module fifo_mem(data_out,fifo_full, fifo_empty, fifo_threshold, fifo_overflow, fifo_underflow,clk, rst_n, wr, rd, data_in);  
-  input wr, rd, clk, rst_n;  
-  input[7:0] data_in;   
-  output[7:0] data_out;  
-  output fifo_full, fifo_empty, fifo_threshold, fifo_overflow, fifo_underflow;  
-  wire[4:0] wptr,rptr;  
-  wire fifo_we,fifo_rd;   
-  write_pointer top1(wptr,fifo_we,wr,fifo_full,clk,rst_n);  
-  read_pointer top2(rptr,fifo_rd,rd,fifo_empty,clk,rst_n);  
-  memory_array top3(data_out, data_in, clk,fifo_we, wptr,rptr);  
-  status_signal top4(fifo_full, fifo_empty, fifo_threshold, fifo_overflow, fifo_underflow, wr, rd, fifo_we, fifo_rd, wptr,rptr,clk,rst_n);  
- endmodule  
+parameter address_bus_length = 4;
+parameter data_bus_length = 8;
+parameter fifo_depth = 2**address_bus_length;
 
-// Verilog code for Memory Array submodule 
- module memory_array(data_out, data_in, clk,fifo_we, wptr,rptr);  
-  input[7:0] data_in;  
-  input clk,fifo_we;  
-  input[4:0] wptr,rptr;  
-  output[7:0] data_out;  
-  reg[7:0] data_out2[15:0];  
-  wire[7:0] data_out;  
-  always @(posedge clk)  
-  begin  
-   if(fifo_we)   
-      data_out2[wptr[3:0]] <=data_in ;  
-  end  
-  assign data_out = data_out2[rptr[3:0]];  
- endmodule  
+input trans_clk,recv_clk,trans_rst,recv_rst;
+input write_enable,read_enable;
+input[data_bus_length-1:0] trans_data;
+output[data_bus_length-1:0] recv_data;
+output fifo_full,fifo_empty;
+
+reg[data_bus_length-1:0] fifo[fifo_depth-1:0];
+
+reg[address_bus_length:0] wptr,rptr;
+wire[address_bus_length:0] wptr_trans_grey,rptr_recv_grey;
+reg[address_bus_length:0] wptr_metarecv,rptr_metatrans;
+reg[address_bus_length:0] wptr_recv_grey,rptr_trans_grey;
+wire[address_bus_length:0] wptr_recv,rptr_trans;
 
 
-// Verilog code for Read Pointer sub-module 
- module read_pointer(rptr,fifo_rd,rd,fifo_empty,clk,rst_n);  
-  input rd,fifo_empty,clk,rst_n;  
-  output[4:0] rptr;  
-  output fifo_rd;  
-  reg[4:0] rptr;  
-  assign fifo_rd = (~fifo_empty)& rd;  
-  always @(posedge clk or negedge rst_n)  
-  begin  
-   if(~rst_n) rptr <= 5'b00000;  
-   else if(fifo_rd)  
-    rptr <= rptr + 5'b00001;  
-   else  
-    rptr <= rptr;  
-  end  
- endmodule  
+always@(posedge trans_clk or negedge trans_rst)
+begin
+if(~trans_rst)
+wptr <=  5'b00000;
+else if((~fifo_full) & write_enable)
+begin
+fifo[wptr[address_bus_length-1:0]] <=  trans_data;
+wptr <=  wptr+1;
+end
+end
 
-// Verilog code for Status Signals sub-module 
- module status_signal(fifo_full, fifo_empty, fifo_threshold, fifo_overflow, fifo_underflow, wr, rd, fifo_we, fifo_rd, wptr,rptr,clk,rst_n);  
-  input wr, rd, fifo_we, fifo_rd,clk,rst_n;  
-  input[4:0] wptr, rptr;  
-  output fifo_full, fifo_empty, fifo_threshold, fifo_overflow, fifo_underflow;  
-  wire fbit_comp, overflow_set, underflow_set;  
-  wire pointer_equal;  
-  wire[4:0] pointer_result;  
-  reg fifo_full, fifo_empty, fifo_threshold, fifo_overflow, fifo_underflow;  
-  assign fbit_comp = wptr[4] ^ rptr[4];  
-  assign pointer_equal = (wptr[3:0] - rptr[3:0]) ? 0:1;  
-  assign pointer_result = wptr[4:0] - rptr[4:0];  
-  assign overflow_set = fifo_full & wr;  
-  assign underflow_set = fifo_empty&rd;  
-  always @(*)  
-  begin  
-   fifo_full =fbit_comp & pointer_equal;  
-   fifo_empty = (~fbit_comp) & pointer_equal;  
-   fifo_threshold = (pointer_result[4]||pointer_result[3]) ? 1:0;  
-  end  
-  always @(posedge clk or negedge rst_n)  
-  begin  
-  if(~rst_n) fifo_overflow <=0;  
-  else if((overflow_set==1)&&(fifo_rd==0))  
-   fifo_overflow <=1;  
-   else if(fifo_rd)  
-    fifo_overflow <=0;  
-    else  
-     fifo_overflow <= fifo_overflow;  
-  end  
-  always @(posedge clk or negedge rst_n)  
-  begin  
-  if(~rst_n) fifo_underflow <=0;  
-  else if((underflow_set==1)&&(fifo_we==0))  
-   fifo_underflow <=1;  
-   else if(fifo_we)  
-    fifo_underflow <=0;  
-    else  
-     fifo_underflow <= fifo_underflow;  
-  end  
- endmodule  
+assign  recv_data  = fifo[rptr[address_bus_length-1:0]];
+always@(posedge recv_clk or negedge recv_rst)
+begin
+if(~recv_rst)
+rptr <=  5'b00000;
+else if((~fifo_empty) & read_enable)
+rptr <=  rptr+1;
+end
 
-// Verilog code for Write Pointer sub-module 
- module write_pointer(wptr,fifo_we,wr,fifo_full,clk,rst_n);  
-  input wr,fifo_full,clk,rst_n;  
-  output[4:0] wptr;  
-  output fifo_we;  
-  reg[4:0] wptr;  
-  assign fifo_we = (~fifo_full)&wr;  
-  always @(posedge clk or negedge rst_n)  
-  begin  
-   if(~rst_n) wptr <= 5'b00000;  
-   else if(fifo_we)  
-    wptr <= wptr + 5'b00001;  
-   else  
-    wptr <= wptr;  
-  end  
- endmodule 
+assign  wptr_trans_grey = wptr^(wptr>>1);
+assign  rptr_recv_grey  = rptr^(rptr>>1);
+
+always@(posedge trans_clk)
+begin
+if(~trans_rst)
+begin
+rptr_metatrans <=  5'b00000;
+rptr_trans_grey <=  5'b00000;
+end
+else
+begin
+rptr_metatrans <=  rptr_recv_grey;
+rptr_trans_grey <=  rptr_metatrans;
+end
+end
+
+always@(posedge recv_clk)
+begin
+if(~recv_rst)
+begin
+wptr_metarecv <=  5'b00000;
+wptr_recv_grey <=  5'b00000;
+end
+else
+begin
+wptr_metarecv <=  wptr_trans_grey;
+wptr_recv_grey <=  wptr_metarecv;
+end
+end
+
+assign  wptr_recv  = wptr_recv_grey^(wptr_recv_grey>>1)^(wptr_recv_grey>>2)^(wptr_recv_grey>>3);
+assign  rptr_trans = rptr_trans_grey^(rptr_trans_grey>>1)^(rptr_trans_grey>>2)^(rptr_trans_grey>>3);
+
+assign  fifo_full  = ((wptr[address_bus_length-1:0] == rptr_trans[address_bus_length-1:0]) && (wptr[address_bus_length] ^ rptr_trans[address_bus_length]));
+assign  fifo_empty = ((rptr[address_bus_length-1:0] == wptr_recv[address_bus_length-1:0]) && (~(rptr[address_bus_length]^wptr_recv[address_bus_length])));
+
+endmodule
